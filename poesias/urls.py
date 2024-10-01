02eae574e407175751e5c92d3f4957d3bcf34cd8
GuilhermeRenno <guilherme.silveirareno@gmail.com>
2024-10-01 19:56:19 -0300

    +Sobre,contatos,produtos

--- poesias/urls.py
+++ poesias/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from poesias.views import my_view,root_view,user_view,home,contexto
+from poesias.views import contatos,root_view,user_view,home,contexto,sobre,produtos
 
 urlpatterns = [
     path('home/', home),
-    path('sobre/', my_view),
+    path('sobre/', sobre),
+    path('contatos/', contatos),
+    path('produtos/', produtos),
     path('user/<str:username>/',user_view),
     path('',root_view),
     path('contexto',contexto)
--- poesias/views.py
+++ poesias/views.py
@@ -2,20 +2,14 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
-def my_view(request):
-    return HttpResponse('''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Pagina</title>
-</head>
-<body>
-    <h1>Bem-vindo a minha página Django</h1>
-    <p>Esta é minha página Servida através do Django</p>
-    
-</body>
-</html>''')
+def contatos(request):
+    return render(request,'contatos.html')
+
+def sobre(request):
+    return render(request,'sobre.html')
+
+def produtos(request):
+    return render(request,'produtos.html')
 
 def user_view(request, username):
     return HttpResponse(f"Perfil do Usuário:{username}")
